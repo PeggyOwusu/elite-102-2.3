5266c3d8c4f4e7ff018d7848facf84f343a7e07e
PeggyOwusu <osaeowusupeggy@gmail.com>
2024-03-28 16:15:53 -0400

    Creating welcom message

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print ('hello')
+print("Hello and welcome to BigBucks Bank.")
+print("-----------------------------------")
+print("\nBefore we begin, please enter your account number. If you dont have an account, please select 'create account.'  ")
