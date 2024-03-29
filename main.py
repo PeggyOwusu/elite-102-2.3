12eb7eefe0ceb029b5d6f0db6d5f667bbcdaf942
PeggyOwusu <osaeowusupeggy@gmail.com>
2024-03-29 19:00:57 -0400

    Added an interactive menu

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+program_loop = True
+
 print("Hello and welcome to BigBucks Bank.")
 print("-----------------------------------")
-print("\nBefore we begin, please enter your account number. If you dont have an account, please select 'create account.'  ")
+
+def bank_menu():
+  print("1. Sign in to bank account\n")
+  print("2. Create account\n")
+
+range = (1, 2)
+
+print("\nBefore we begin, please sign in to your account. If you don't have an account, please select option 2. \n")
+def user_selection():
+  user_selection = int(input("\nSelect an option by typing a number:"))
+  return user_selection
+
+def sign_in():
+  acc_num = input("Enter account number:")
+  acc_pin = input("Enter account pin:")
+  
+def create_acc(): 
+  print("c")
+
+
+if user_selection == "1":
+  sign_in()
+elif user_selection == "2":
+  create_acc()
+
+bank_menu()
+while program_loop:
+  option = user_selection()
+
+  if option in range:
+    if option == 1:
+      sign_in()
+    elif option == 2:
+      create_acc()
